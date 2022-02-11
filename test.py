2dcda33f44d505e56c723171da680f11d0d39952
Rafael Almeida <rafaelpbcp@gmail.com>
2022-02-11 02:20:17 -0500

    minor changes

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 import requests
 
-problems = requests.get("https://www.aigocode.org/api/queries/allproblems")
-print(problems.json()[-1])
+json = {
+    # TODO replace this with a env variable
+    "key": '7e8e71c8-0308-4dcc-b09e-2b00dbec60c9',
+    "icode": "print('hello')",
+    "ilanguage": "python3",
+    "iversion": "3.9.4",
+    "idiscordid": "250782339758555136",
+    "problemid": "61fe72fe83d30c0009a33899",
+    "iextension": "py"
+}
+
+response = requests.post("https://codingcomp.netlify.app/api/bot/solve", json=json)
+print(response.text)
