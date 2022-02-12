6b21d7cbaf5c732766b11d60feb80fcc26113c3f
Rafael Almeida <rafaelpbcp@gmail.com>
2022-02-12 10:47:33 -0500

    added stats

--- cogs/features/stats.py
+++ cogs/features/stats.py
@@ -3,11 +3,19 @@ from discord.ext import commands
 
 from cogs.helpers import Helpers
 from cogs.db.db_management import DB
-from .feature_login import Feature
+from .feature_cog import Feature
 
 class StatsCommands(Feature):
     def __init__(self, bot):
         super().__init__(bot, 'stats', StatsView)
+
+    @commands.command()
+    async def add_stats(self, ctx):
+        await super().add_feature(ctx)
+
+    @commands.command()
+    async def remove_stats(self, ctx):
+        await super().remove_feature(ctx)
 
 class StatsView(discord.ui.View):
     def __init__(self):
