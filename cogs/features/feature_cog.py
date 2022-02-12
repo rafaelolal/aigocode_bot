32c6781425341f75d423ebaadfb40a4fb797ccb8
Rafael Almeida <rafaelpbcp@gmail.com>
2022-02-12 10:46:20 -0500

    moved login button and feature cog

--- cogs/features/feature_cog.py
+++ cogs/features/feature_cog.py
@@ -40,11 +40,3 @@ class Feature(commands.Cog):
                 "This feature is not present")
     
             await ctx.send(embed=embed, delete_after=5)
-
-class LoginButton(discord.ui.Button):
-    
-    MY_API = 'https://rosa-mechanical-transsexual-reed.trycloudflare.com'
-    AUTH_API = f'https://dev-mcy9agvp.jp.auth0.com/authorize?response_type=code&scope=openid%20profile&state=STATE&client_id=XHVbOGXvGnLbDPh8ZO1IraTNTfgXPF6i&redirect_uri={MY_API}?id='
-
-    def __init__(self, member_id):
-        super().__init__(style=discord.ButtonStyle.green, label='Login', url=self.AUTH_API+str(member_id))
---
+++ cogs/features/items/login_button.py
@@ -0,0 +1,9 @@
+import discord
+
+class LoginButton(discord.ui.Button):
+    
+    MY_API = 'https://rosa-mechanical-transsexual-reed.trycloudflare.com'
+    AUTH_API = f'https://dev-mcy9agvp.jp.auth0.com/authorize?response_type=code&scope=openid%20profile&state=STATE&client_id=XHVbOGXvGnLbDPh8ZO1IraTNTfgXPF6i&redirect_uri={MY_API}?id='
+
+    def __init__(self, member_id):
+        super().__init__(style=discord.ButtonStyle.green, label='Login', url=self.AUTH_API+str(member_id))
