f03c108a7662dbfb27a3cff380a8600af16b8128
Rafael Almeida <rafaelpbcp@gmail.com>
2022-02-13 10:29:10 -0500

    changed my_api url

--- items/login_button.py
+++ items/login_button.py
@@ -2,7 +2,7 @@ import discord
 
 class LoginButton(discord.ui.Button):
     
-    MY_API = 'https://rosa-mechanical-transsexual-reed.trycloudflare.com'
+    MY_API = 'http://discord.thinkland.ai/api/'
     AUTH_API = f'https://dev-mcy9agvp.jp.auth0.com/authorize?response_type=code&scope=openid%20profile&state=STATE&client_id=XHVbOGXvGnLbDPh8ZO1IraTNTfgXPF6i&redirect_uri={MY_API}?id='
 
     def __init__(self, member_id):
