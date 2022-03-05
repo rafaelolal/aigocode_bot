37693c4e18e84977e64633176d92c8f850423dad
Rafael Almeida <rafaelpbcp@gmail.com>
2022-03-05 14:36:56 -0500

    Using embeds instead of regular messages

--- cogs/features/features/display.py
+++ cogs/features/features/display.py
@@ -1,7 +1,7 @@
 import validators
 
 from discord.ext import commands
-from discord import Embed
+from discord import Colour, Embed
 import discord
 
 from .feature_cog import Feature
@@ -21,13 +21,24 @@ class DisplayCommands(Feature):
 class DisplayView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
+        self.creating_now = []
+
+    async def interaction_check(self, interaction) -> bool:
+        if interaction.user in self.creating_now:
+            await interaction.response.send_message("Finish creating before trying again", ephemeral=True)
+            return False
+
+        return True
 
     @discord.ui.button(label="Display a Project", style=discord.ButtonStyle.blurple, custom_id='project_display')
     async def display(self, button, interaction):
         await interaction.response.defer()
 
+        self.creating_now.append(interaction.user)
+
         user = interaction.user
         embed = await self.ask_to(user)
+        self.creating_now.remove(interaction.user)
         if not embed:
             return
 
@@ -85,7 +96,7 @@ class User:
         await prompt.wait()
         if prompt.is_confirmed is True:
             response_msg = await prompt.get_response(request_msg.channel)
-            await prompt.acknowledge(self.user, response_msg)
+            await prompt.acknowledge(self.user, response_msg, request_msg)
 
         elif prompt.is_confirmed is None:
             if prompt.prompt_type == 'link':
@@ -97,10 +108,8 @@ class User:
             return eval(f'embed.{prompt.prompt_type}')
 
         else:
-            await self.user.send("Goodbye!")
             return
 
-        # TODO i dont like this because I createed acknowledge to avoid checking the type of prompt, but now I gotta check anyways
         if isinstance(prompt, ImagePrompt):
             return response_msg.attachments[-1].url
 
@@ -116,11 +125,11 @@ class EditProjectView(discord.ui.View):
         
         ID = 0
         if interaction.user.id != owner[ID] and interaction.user.id != interaction.guild.owner_id:
-            await interaction.response.send_message("Only the owner can do this.", ephemeral=True)
+            await interaction.response.send_message("Only the owner can do this", ephemeral=True)
             return False
 
         elif interaction.user in self.editing_now:
-            await interaction.response.send_message("Finish editing before trying again.", ephemeral=True)
+            await interaction.response.send_message("Finish editing before trying again", ephemeral=True)
             return False
 
         return True
@@ -153,7 +162,7 @@ class Prompt(discord.ui.View):
         super().__init__(timeout=None)
         self.is_confirmed = None
         self.prompt_type = prompt_type
-        self.error_msg = f"Send a {self.prompt_type} before confirming."
+        self.error_msg = f"Send a {self.prompt_type} before confirming"
 
         if editing:
             self.add_item(KeepButton())
@@ -161,28 +170,56 @@ class Prompt(discord.ui.View):
     @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
     async def confirm(self, button, interaction):
         if not await self.is_valid(interaction):
-            await self.send_error_msg(interaction)
+            await self.add_error_field(interaction)
 
         else:
             self.is_confirmed = True
-            self.stop()
+            await self.disable(interaction)
 
     @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
     async def cancel(self, button, interaction):
+        embed = interaction.message.embeds[0]
+        embed.title = 'Process Canceled'
+        embed.description = 'I hope to see you displaying other projects!'
+        embed.remove_footer()
+        embed.clear_fields()
+        embed.colour = Colour.light_grey()
+        await interaction.message.edit(embed=embed)
+
         self.is_confirmed = False
-        self.stop()
+        await self.disable(interaction)
     
     @staticmethod
     async def is_valid(interaction) -> bool:
         msg = await Prompt.get_response(interaction.channel)
         return msg != interaction.message
 
-    async def send_error_msg(self, interaction) -> None:
-        if self.error_msg not in interaction.message.content:
-            await interaction.message.edit(content=interaction.message.content+f"\n*{self.error_msg}*")
+    async def add_error_field(self, interaction) -> None:
+        embed = interaction.message.embeds[0]
+        if 'Error' not in [field.name for field in embed.fields]:
+            embed.add_field(name='Error',
+                value=self.error_msg)
+
+            embed.colour = Colour.red()
+
+            await interaction.message.edit(embed=embed)
+
+    async def acknowledge(self, user: Member, response: Message, request: Message) -> None:
+        embed = request.embeds[0]
+
+        if self.prompt_type not in ('image', 'link'):
+            embed.add_field(name="You Entered",
+                value=response.content)
         
-    async def acknowledge(self, user: Member, response: Message) -> None:
-        await user.send(f"You entered: {response.content}")
+        else:
+            embed.url = request.content if self.prompt_type == 'link' else response.attachments[-1].url
+            embed.add_field(name="You Entered",
+                value="Click on the title to preview your response")
+        
+        embed.set_footer(text='Remember you can always edit this later')
+        embed.colour = Colour.green()
+
+        await request.edit(embed=embed)
 
     @staticmethod
     async def get_response(channel: TextChannel) -> Message:
@@ -192,15 +229,26 @@ class Prompt(discord.ui.View):
         return msg
 
     async def send_request_msg_to(self, user: Member) -> Message:
-        return await user.send(f"Respond with the {self.prompt_type} of your project. If you make a mistake, edit or send a new message then confirm.",
-            view=self)
+        embed = Embed(title=self.prompt_type.capitalize(),
+            description=f'Respond with the {self.prompt_type} of your project')
+        embed.set_footer(text='If you make a mistake, edit or send a new message then confirm')
+        embed.colour = Colour.blue()
+
+        return await user.send(embed=embed, view=self)
+
+    async def disable(self, interaction) -> None:
+        for child in self.children:
+            child.disabled = True
+
+        self.stop()
+        await interaction.message.edit(view=None)
 
 class KeepButton(discord.ui.Button):
     def __init__(self):
         super().__init__(style=discord.ButtonStyle.blurple, label='Keep')
 
     async def callback(self, interaction):
-        self.view.stop()
+        await self.view.disable(interaction)
 
 class TitlePrompt(Prompt):
     def __init__(self, editing):
@@ -214,29 +262,21 @@ class ImagePrompt(Prompt):
     def __init__(self, editing):
         super().__init__("image", editing)
 
-        self.error_msg = "Attach and send an image before confirming."
+        self.error_msg = "Attach and send an image before confirming"
 
     async def is_valid(self, interaction) -> bool:
         msg = await self.get_response(interaction.channel)
         return msg != interaction.message and len(msg.attachments) != 0
 
-    async def acknowledge(self, user, response: Message) -> None:
-        image_url = response.attachments[-1].url
-        await user.send(f"The image you attached is: \n> <{image_url}>")
-
 class LinkPrompt(Prompt):
     def __init__(self, editing):
         super().__init__("link", editing)
 
-        self.error_msg = 'Enter a valid "http" link before confirming.'
+        self.error_msg = 'Enter a valid "http" link before confirming'
 
     async def is_valid(self, interaction) -> bool:
         msg = await self.get_response(interaction.channel)
         return validators.url(msg.content) and msg != interaction.message
-
-    async def acknowledge(self, user, response: Message) -> None:
-        response = response.content
-        await user.send(f"You entered: <{response}>")
 
 def setup(bot):
     bot.add_cog(DisplayCommands(bot))
